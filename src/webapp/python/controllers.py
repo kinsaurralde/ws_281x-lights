cbaa64c951e49d1cf9f2b2f789a0bbaa94f47a02
Kyle Insaurralde <kinsaurralde@yahoo.com>
2020-09-11 18:43:19 -0700

    send to controllers using multithreading

--- src/webapp/python/controllers.py
+++ src/webapp/python/controllers.py
@@ -1,5 +1,6 @@
 import json
 import time
+import threading
 import requests
 
 
@@ -8,29 +9,44 @@ class Controllers:
         self.nosend = nosend
         self.send_counter = 0
         self.urls = {}
+        self.fails = []
         self.config = self._setupConfig(config["controllers"])
         self.getControllerLatencies()
 
     def _setupConfig(self, controllers):
         id_counter = 0
         configs = {}
+        threads = []
         for controller in controllers:
             url = controller["url"]
             if url not in self.urls:
                 self.urls[url] = 0
-            self._send(
-                url + "/init",
-                {"id": controller["strip_id"], "init": controller["init"]},
+            threads.append(
+                self._send(
+                    url + "/init",
+                    {"id": controller["strip_id"], "init": controller["init"]},
+                )
             )
             controller["id"] = id_counter
             configs[controller["name"]] = controller
+        for thread in threads:
+            thread.join()
         return configs
 
     def _send(self, url, payload=None, controller_id=None):
+        thread = threading.Thread(
+            target=self._sending_thread, args=(url, payload, controller_id)
+        )
+        thread.start()
+        return thread
+
+    def _sending_thread(self, url, payload=None, controller_id=None):
         self.send_counter += 1
         if self.nosend:
             print(f"Would have sent to {url}:\n{payload}")
-            return {"url": url, "id": controller_id, "message": "No send is true"}
+            self.fails.append(
+                {"url": url, "id": controller_id, "message": "No send is true"}
+            )
         try:
             if payload is None:
                 requests.get(url)
@@ -38,13 +54,14 @@ class Controllers:
                 requests.post(url, data=json.dumps(payload), timeout=0.5)
         except requests.RequestException:
             print(f"Failed to send to {url}")
-            return {"url": url, "id": controller_id, "message": "Connection Error"}
-        return None
+            self.fails.append(
+                {"url": url, "id": controller_id, "message": "Connection Error"}
+            )
 
     def getControllerLatencies(self):
         for url in self.urls:
             start_time = time.time()
-            self._send(url)
+            self._sending_thread(url)
             end_time = time.time()
             latency = int((end_time - start_time) * 1000)
             previous = self.urls[url]
@@ -58,12 +75,12 @@ class Controllers:
 
     def send(self, commands):
         queue = {}
-        fails = []
+        self.fails = []
         total_estimated_latency = 0
         for command in commands:
             controller_name = command["id"]
             if controller_name not in self.config:
-                fails.append(
+                self.fails.append(
                     {
                         "url": "localhost",
                         "id": controller_name,
@@ -77,17 +94,17 @@ class Controllers:
                 total_estimated_latency += self.urls[url]
                 queue[url] = []
             queue[url].append(command)
+        threads = []
         for url in queue:
             total_estimated_latency -= self.urls[url]
-            print(f"Delay for {url} is {total_estimated_latency}")
             for command in queue[url]:
                 command["delay"] = total_estimated_latency
-            response = self._send(url + "/data", queue[url], queue[url][0]["id"])
-            if response is not None:
-                fails.append(response)
-        if self.send_counter % 50 == 0:
+            threads.append(self._send(url + "/data", queue[url], queue[url][0]["id"]))
+        for thread in threads:
+            thread.join()
+        if self.send_counter % 25 == 0:
             self.getControllerLatencies()
-        return fails
+        return self.fails
 
     def getConfig(self):
         return self.config
