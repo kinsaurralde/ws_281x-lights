dcda7d690ca4cb3bbb758c36df012eb2f2e36ca8
Kyle Insaurralde <kinsaurralde@yahoo.com>
2020-09-15 16:24:07 -0700

    add status table to show version, initialized, connected (not implemented yet), and Hash match

--- src/webapp/python/controllers.py
+++ src/webapp/python/controllers.py
@@ -12,9 +12,9 @@ class Controllers:
         self.nosend = nosend
         self.send_counter = 0
         self.urls = {}
+        self.latencies = {}
         self.brightness_queue = {}
         self.brightness_timer_active = False
-        self.fails = []
         self.config = self._setupConfig(config["controllers"])
         self.getControllerLatencies()
 
@@ -24,7 +24,9 @@ class Controllers:
         for controller in controllers:
             url = controller["url"]
             if url not in self.urls:
-                self.urls[url] = 0
+                self.urls[url] = []
+                self.latencies[url] = 0
+            self.urls[url].append(controller["name"])
             self.initController(url, controller)
             controller["id"] = id_counter
             configs[controller["name"]] = controller
@@ -32,21 +34,23 @@ class Controllers:
 
     def initController(self, url, controller):
         self._send(
-            url + "/init", {"id": controller["strip_id"], "init": controller["init"]},
+            [],
+            url + "/init",
+            {"id": controller["strip_id"], "init": controller["init"]},
         )
 
-    def _send(self, url, payload=None, controller_id=None):
+    def _send(self, fails, url, payload=None, controller_id=None):
         thread = threading.Thread(
-            target=self._sending_thread, args=(url, payload, controller_id)
+            target=self._sending_thread, args=(fails, url, payload, controller_id)
         )
         thread.start()
         return thread
 
-    def _sending_thread(self, url, payload=None, controller_id=None):
+    def _sending_thread(self, fails, url, payload=None, controller_id=None):
         self.send_counter += 1
         if self.nosend:
             print(f"Would have sent to {url}:\n{payload}")
-            self.fails.append(
+            fails.append(
                 {"url": url, "id": controller_id, "message": "No send is true"}
             )
         try:
@@ -55,38 +59,40 @@ class Controllers:
             return requests.post(url, data=json.dumps(payload), timeout=0.5)
         except requests.RequestException:
             print(f"Failed to send to {url}")
-            self.fails.append(
+            fails.append(
                 {"url": url, "id": controller_id, "message": "Connection Error"}
             )
         return []
 
     def getControllerLatencies(self):
-        for url in self.urls:
+        for url in self.latencies:
             start_time = time.time()
-            self._sending_thread(url)
+            self._sending_thread([], url)
             end_time = time.time()
             latency = int((end_time - start_time) * 1000)
-            previous = self.urls[url]
+            previous = self.latencies[url]
             if previous == 0:
                 previous = latency
-            self.urls[url] = (previous + latency) / 2
-        print("Controller Latencies", self.urls)
+            self.latencies[url] = (previous + latency) / 2
+        print("Controller Latencies", self.latencies)
 
     def getControllerVersionInfo(self):
-        self.fails = []
-        data = []
+        fails = []
+        data = {}
         version_match = True
         hash_match = True
         for url in self.urls:
-            response = self._sending_thread(url + "/versioninfo").json()
-            data.append(response)
+            response = self._sending_thread(fails, url + "/versioninfo").json()
+            for controller in self.urls[url]:
+                data[controller] = response
+            # data.append(response)
             if (
                 response["major"] != self.version_info["major"]
                 or response["minor"] != self.version_info["minor"]
                 or response["patch"] != self.version_info["patch"]
             ):
                 version_match = False
-                self.fails.append(
+                fails.append(
                     {"url": url, "id": "version", "message": "Version doesnt match"}
                 )
             if (
@@ -94,27 +100,25 @@ class Controllers:
                 or response["rpi_hash"] != self.version_info["rpi_hash"]
             ):
                 hash_match = False
-                self.fails.append(
-                    {"url": url, "id": "hash", "message": "Hash doesnt match"}
-                )
+                fails.append({"url": url, "id": "hash", "message": "Hash doesnt match"})
         return {
             "versioninfo": data,
-            "fails": self.fails,
+            "fails": fails,
             "webapp": self.version_info,
             "version_match": version_match,
             "hash_match": hash_match,
         }
 
     def getControllerInitialized(self):
-        self.fails = []
+        fails = []
         data = {}
         for url in self.urls:
-            response = self._sending_thread(url + "/init").json()
+            response = self._sending_thread(fails, url + "/init").json()
             for controller in self.config:
                 if self.config[controller]["url"] == url:
                     response_index = int(self.config[controller]["strip_id"])
                     if response_index < 0 or response_index >= len(response):
-                        self.fails.append(
+                        fails.append(
                             {
                                 "url": url,
                                 "id": response_index,
@@ -123,19 +127,19 @@ class Controllers:
                         )
                     else:
                         data[controller] = response[response_index]
-        return {"fails": self.fails, "initialized": data}
+        return {"fails": fails, "initialized": data}
 
     def setNoSend(self, value):
         self.nosend = value
 
     def send(self, commands):
         queue = {}
-        self.fails = []
+        fails = []
         total_estimated_latency = 0
         for command in commands:
             controller_name = command["id"]
             if controller_name not in self.config:
-                self.fails.append(
+                fails.append(
                     {
                         "url": "localhost",
                         "id": controller_name,
@@ -146,20 +150,22 @@ class Controllers:
             command["id"] = self.config[controller_name]["strip_id"]
             url = self.config[controller_name]["url"]
             if url not in queue:
-                total_estimated_latency += self.urls[url]
+                total_estimated_latency += self.latencies[url]
                 queue[url] = []
             queue[url].append(command)
         threads = []
         for url in queue:
-            total_estimated_latency -= self.urls[url]
+            total_estimated_latency -= self.latencies[url]
             for command in queue[url]:
                 command["delay"] = total_estimated_latency
-            threads.append(self._send(url + "/data", queue[url], queue[url][0]["id"]))
+            threads.append(
+                self._send(fails, url + "/data", queue[url], queue[url][0]["id"])
+            )
         for thread in threads:
             thread.join()
         if self.send_counter % 25 == 0:
             self.getControllerLatencies()
-        return self.fails
+        return fails
 
     def getConfig(self):
         return self.config
@@ -168,7 +174,7 @@ class Controllers:
         time.sleep(BRIGHTNESS_BUFFER_TIMER)
         while len(self.brightness_queue) > 0:
             name = list(self.brightness_queue.keys())[0]
-            self._send(self.brightness_queue[name])
+            self._send([], self.brightness_queue[name])
             self.brightness_queue.pop(name)
         self.brightness_timer_active = False
 
--- src/webapp/static/controllers.js
+++ src/webapp/static/controllers.js
@@ -2,21 +2,28 @@
 /* globals socket */
 
 const TABLE_COLUMNS = 6;
+const STATUS_COLUMNS = 5;
 
 const GOOD = 'GOOD';
 const WARNING = 'WARNING';
 const ERROR = 'ERROR';
+const FALSE = 'FALSE';
+const TRUE = 'TRUE';
+const UNKNOWN = 'UNKNOWN';
 
 class Controllers {
   constructor() {
     const table = document.getElementById('controller-table');
+    const status_table = document.getElementById('status-table');
     this.table = table.getElementsByTagName('tbody')[0];
+    this.status_table = status_table.getElementsByTagName('tbody')[0];
     this.num_controllers = 0;
     this.num_strips = 0;
     this.controllers = {};
     this.brightness_link = [];
     this.fetchControllers();
     this.fetchInitialized();
+    this.fetchVersionInfo();
   }
 
   fetchControllers() {
@@ -42,7 +49,7 @@ class Controllers {
             if (name in initialized.initialized) {
               const status =
                   document.getElementById(`controllers-table-${name}-status`);
-              console.log(initialized.initialized);
+              this.setStatusInitialized(name, initialized.initialized[name]);
               if (initialized.initialized[name]) {
                 this.setStatus(status, GOOD);
               } else {
@@ -53,17 +60,75 @@ class Controllers {
         });
   }
 
-  setStatus(div, type) {
+  fetchVersionInfo() {
+    fetch('/getversioninfo')
+        .then((response) => response.json())
+        .then((version_info) => {
+          console.log('Recieved Version Info', version_info);
+          const major = version_info.webapp.major;
+          const minor = version_info.webapp.minor;
+          const patch = version_info.webapp.patch;
+          const version = `${major}.${minor}.${patch}`;
+          const esp_hash = version_info.webapp.esp_hash;
+          const rpi_hash = version_info.webapp.rpi_hash;
+          document.getElementById('status-webapp-version').textContent =
+              version;
+          document.getElementById('status-webapp-esphash').textContent =
+              esp_hash;
+          document.getElementById('status-webapp-rpihash').textContent =
+              rpi_hash;
+          console.log(version_info.versioninfo);
+          for (const controller in version_info.versioninfo) {
+            if (controller in version_info.versioninfo) {
+              const data = version_info.versioninfo[controller];
+              const version_string =
+                  `${data.major}.${data.minor}.${data.patch}`;
+              const version_match =
+                  (major === data.major && minor === data.minor);
+              const full_version_match =
+                  (version_match && patch === data.patch);
+              const status =
+                  full_version_match ? GOOD : version_match ? WARNING : ERROR;
+              this.setStatusVersion(controller, status, version_string);
+              const hash_match =
+                  esp_hash === data.esp_hash && rpi_hash === data.rpi_hash;
+              this.setStatusHashMatch(controller, hash_match);
+              console.log(data);
+            }
+          }
+        });
+  }
+
+  setStatus(div, type, text = null) {
     div.classList.remove(...div.classList);
     div.classList.add('section-title-secondary');
-    div.textContent = type;
+    if (text === null) {
+      div.textContent = type;
+    } else {
+      div.textContent = text;
+    }
     if (type === WARNING) {
       div.classList.add('yellow');
-    } else if (type === GOOD) {
+    } else if (type === GOOD || type === TRUE) {
       div.classList.add('green');
-    } else {  // type === ERROR
+    } else {
       div.classList.add('red');
     }
+  }
+
+  setStatusInitialized(name, value) {
+    const div = document.getElementById(`status-table-${name}-initialized`);
+    this.setStatus(div, value ? TRUE : FALSE);
+  }
+
+  setStatusVersion(name, value, text) {
+    const div = document.getElementById(`status-table-${name}-version`);
+    this.setStatus(div, value, text);
+  }
+
+  setStatusHashMatch(name, value) {
+    const div = document.getElementById(`status-table-${name}-hashmatch`);
+    this.setStatus(div, value ? TRUE : FALSE);
   }
 
   getNames() {
@@ -104,7 +169,7 @@ class Controllers {
 
     const status = document.createElement('div');
     status.id = id + '-status';
-    this.setStatus(status, 'UNKNOWN');
+    this.setStatus(status, UNKNOWN);
 
     const index = this.num_strips - 1;
     box.addEventListener('input', () => {
@@ -135,5 +200,36 @@ class Controllers {
     cells[3].textContent = controller.init.milliwatts;
     cells[4].textContent = this.num_strips - 1;
     cells[5].appendChild(status);
+
+    this.addStatusRow(controller.name);
+  }
+
+  addStatusRow(name) {
+    const row = this.status_table.insertRow();
+    const cells = [];
+    for (let i = 0; i < STATUS_COLUMNS; i++) {
+      cells.push(row.insertCell());
+    }
+    const id = 'status-table-' + name;
+    const display_name = createSecondTitle(id + '-name', name);
+    const initialized = document.createElement('div');
+    const connected = document.createElement('div');
+    const version = document.createElement('div');
+    const hash_match = document.createElement('div');
+    this.setStatus(initialized, UNKNOWN);
+    this.setStatus(connected, UNKNOWN);
+    this.setStatus(version, UNKNOWN);
+    this.setStatus(hash_match, UNKNOWN);
+
+    initialized.id = id + '-initialized';
+    connected.id = id + '-connected';
+    version.id = id + '-version';
+    hash_match.id = id + '-hashmatch';
+
+    cells[0].appendChild(display_name);
+    cells[1].appendChild(initialized);
+    cells[2].appendChild(connected);
+    cells[3].appendChild(version);
+    cells[4].appendChild(hash_match);
   }
 }
