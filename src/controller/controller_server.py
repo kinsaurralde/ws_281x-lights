7fb9e1de3c54235b0a84763793c5081c4b21f7b2
Kyle Insaurralde <kinsaurralde@yahoo.com>
2020-09-25 10:49:09 -0700

    add comments to controller_server.py

--- src/controller/controller_server.py
+++ src/controller/controller_server.py
@@ -38,11 +38,45 @@ def create_response(data):
     return response
 
 
+# /**
+#  * Route: /
+#  * Methods: GET
+#  * 
+#  * Should only be called for ping or connected check
+#  */ 
 @app.route("/")
 def handleRoot():
     return "Hello!"
 
 
+# /**
+#  * Route: /init
+#  * Methods: GET, POST
+#  * 
+#  * POST JSON Properties:
+#  *      - unsigned int id:              id of strip to initialize
+#  *      - unsigned int init.brightness: initial brightness value
+#  *      - unsigned int init.num_leds:   number of pixels on strip
+#  *      - unsigned int init.milliwatts: maximum milliwatts (not implemented)
+#  * 
+#  * Initialze strip with brightness, number of leds, 
+#  */
+@app.route("/init", methods=["GET", "POST"])
+def init():
+    if request.method == 'GET':
+        return create_response(neo.getInit())
+    return create_response(neo.init(json.loads(request.data)))
+
+
+# /**
+#  * Route: /data 
+#  * Methods: POST
+#  * 
+#  * Parameters:
+#  *      - Array of commands, command properties in docs
+#  * 
+#  * Run animation commands
+#  */
 @app.route("/data", methods=["POST"])
 def handleData():
     print("Data" , request.data)
@@ -50,20 +84,38 @@ def handleData():
     return "[]"
 
 
+# /**
+#  * Route: /brightness
+#  * Methods: GET
+#  * 
+#  * Query Parameters:
+#  *      - unsigned integer id:    which strip to read/change
+#  *      - unsigned integer value: new brightness value (optional)
+#  * 
+#  * Sets brightness of strip id to value (if exists) then returns current value
+#  */
+@app.route("/brightness")
+def handleBrightness():
+    return create_response(neo.handleBrightness(request.args.get('id'), request.args.get('value')))
+
+
+# /**
+#  * Route: /getpixels 
+#  * Methods: GET
+#  * 
+#  * Returns json array of each pixel for each strip
+#  */
 @app.route("/getpixels")
 def handleGetPixels():
     return create_response(neo.getPixels())
 
 
-@app.route("/heapfree")
-def handleFreeHeap():
-    return "-1"
-
-
-@app.route("/brightness")
-def handleBrightness():
-    return create_response(neo.handleBrightness(request.args.get('id'), request.args.get('value')))
-
+# /**
+#  * Route: /versioninfo 
+#  * Methods: GET
+#  * 
+#  * Return version info of this controller
+#  */
 @app.route("/versioninfo")
 def versioninfo():
     return create_response({
@@ -74,19 +126,38 @@ def versioninfo():
         "rpi_hash": version.RPI_HASH,
     })
 
+
+# /**
+#  * Route:
+#  * Methods: GET
+#  * 
+#  * Not used for this controller
+#  */
+@app.route("/heapfree")
+def handleFreeHeap():
+    return "-1"
+
+
+# /**
+#  * Route:
+#  * Methods: GET
+#  * 
+#  * Not used for this controller
+#  */
 @app.route("/ledon")
 def ledon():
     return "On"
 
+
+# /**
+#  * Route:
+#  * Methods: GET
+#  * 
+#  * Not used for this controller
+#  */
 @app.route("/ledoff")
 def ledoff():
     return "Off"
-
-@app.route("/init", methods=["GET", "POST"])
-def init():
-    if request.method == 'GET':
-        return create_response(neo.getInit())
-    return create_response(neo.init(json.loads(request.data)))
 
 
 if __name__ == "__main__":
--- src/controller/version.h
+++ src/controller/version.h
@@ -5,4 +5,4 @@
 #define PATCH 0
 
 #define ESP_HASH "9476dd56d2dad50ab9c3bc2070cd018db495d3f3"
-#define RPI_HASH "289868b38c9d267c002280cebe036f539ca1c5d3"
+#define RPI_HASH "d58a87daf9f13b395500166833e45d061cf9b85d"
