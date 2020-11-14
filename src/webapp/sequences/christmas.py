824a54960eec437a9a29b8e82989c684f8a1020c
Kyle Insaurralde <kinsaurralde@yahoo.com>
2020-11-14 00:31:31 -0800

    add links / add sample sequence

---
+++ src/webapp/sequences/christmas.py
@@ -0,0 +1,39 @@
+import time
+
+from sequences.sequence_base import SequenceBase, Preset
+
+class Sequence(SequenceBase):
+    def __init__(self, sequencer, send, config) -> None:
+        super().__init__(sequencer, send, config)
+
+    def basic(self):
+        self.pulse("a", **RED_GREEN_ALTERATE)
+        self.pulse("c", **WHITE_BLUE_ALTERNATE)
+        self.sleep(10)
+        self.pulse("c", **WHITE_BLUE_ALTERNATE_R)
+        self.sleep(10)
+
+    
+RED_GREEN_ALTERATE = {
+    "colors": ["red", "green"],
+    "length": 1,
+    "spacing": 0,
+    "wait_ms": 5000
+}
+
+WHITE_BLUE_ALTERNATE = {
+    "colors": ["blue", "white"],
+    "length": 15,
+    "spacing": 0,
+    "wait_ms": 75,
+    "reverse": False
+}
+
+WHITE_BLUE_ALTERNATE_R = {
+    "colors": ["blue", "white"],
+    "length": 15,
+    "spacing": 0,
+    "wait_ms": 75,
+    "reverse": True
+}
+        
