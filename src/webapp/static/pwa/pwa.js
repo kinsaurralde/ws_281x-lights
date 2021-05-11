b160226828c0f810a4a7afee1e07cf6c8110a977
Kyle Insaurralde <kinsaurralde@yahoo.com>
2021-05-10 22:35:32 -0700

    add expanded color page

--- src/webapp/static/pwa/pwa.js
+++ src/webapp/static/pwa/pwa.js
@@ -39,21 +39,21 @@ document.getElementById('close-expanded-button')
 
 function createAnimationArgs() {
   return {
-    'animation': 0,
-    'color': 0,
-    'color_bg': 0,
-    'colors': [],
-    'arg1': 0,
-    'arg2': 0,
-    'arg3': 0,
-    'arg4': 0,
-    'arg5': 0,
-    'arg6': false,
-    'arg7': false,
-    'arg8': false,
-    'wait_ms': 40,
-    'inc_steps': 1,
-    'id': 'a',
+    animation: 0,
+    color: 0,
+    color_bg: 0,
+    colors: [],
+    arg1: 0,
+    arg2: 0,
+    arg3: 0,
+    arg4: 0,
+    arg5: 0,
+    arg6: false,
+    arg7: false,
+    arg8: false,
+    wait_ms: 40,
+    inc_steps: 1,
+    id: 'a',
   };
 }
 
@@ -69,10 +69,8 @@ function simpleColor(r, g, b) {
 }
 
 function simpleAnimation(args) {
-  console.log('SIMPLE', args);
   const payload = createAnimationArgs();
   for (const arg of Object.keys(args)) {
-    console.log('A', arg, arg in payload);
     if (arg in payload) {
       payload[arg] = args[arg];
     }
