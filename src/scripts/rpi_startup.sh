9412608d06d6f038c1d9ecfa325aca241fd47f6a
Kyle Insaurralde <kinsaurralde@yahoo.com>
2021-10-10 16:00:54 -0700

    delete old

--- src/scripts/rpi_startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}" # change directory to this files directory
-sudo screen -dmS rgb_server sudo python3 controller_server.py "$@"
--- src/webapp/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .controllers import Controllers
-from .background import Background
-from .sequencer import Sequencer
-from .scheduler import Scheduler
-from .extras import CustomLogger
